0ab91227ae08d16e7597fa22bca562062e499130
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2022-12-15 00:42:46 +0200

    UserRepository

---
+++ src/main/java/com/marinoricardo/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.marinoricardo.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.marinoricardo.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    // retornas tasks do user
+    @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    List<Task> findByUser_Id(@Param("id") Long id);
+}
