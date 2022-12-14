1228dd37cf2be8ba97af447d49af39357af52f9b
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2022-12-14 23:58:57 +0200

    Model User

--- src/main/java/com/marinoricardo/todosimple/models/User.java
+++ src/main/java/com/marinoricardo/todosimple/models/User.java
@@ -3,6 +3,9 @@ package com.marinoricardo.todosimple.models;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -39,6 +42,7 @@ public class User {
     @Size(groups = CreateUser.class,min = 2, max = 100)
     private String username;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
