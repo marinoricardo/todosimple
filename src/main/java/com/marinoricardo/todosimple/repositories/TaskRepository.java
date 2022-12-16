92b6a1f694e4ed48be7d11dc307c0f055585f3fa
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2022-12-16 23:14:50 +0200

    Create UserController

--- src/main/java/com/marinoricardo/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/marinoricardo/todosimple/repositories/TaskRepository.java
@@ -3,8 +3,8 @@ package com.marinoricardo.todosimple.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.marinoricardo.todosimple.models.Task;
